0be8221062a024c0caa5fea1f08774d7acc49f0e
qiya770 <599677728@qq.com>
2022-05-22 14:53:42 +0800

    first update

---
+++ src/pages/Register/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        注册test
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Register'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+//使用插件
+Vue.use(VueRouter);
+
+//引入路由组件
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+
+
+
+//配置路由
+
+
+
+
+
